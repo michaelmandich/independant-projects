8cff14e7e8a77d739117d96854b479b3c015046b
Michael Mandich <85703638+michaelmandich@users.noreply.github.com>
2023-02-05 19:59:41 -0500

    Update SpreadReader.py
    added comments

--- SpreadReader.py
+++ SpreadReader.py
@@ -2,23 +2,22 @@ import openpyxl
 from openpyxl import Workbook
 from datetime import datetime
 from datetime import timedelta
-from dateutil import parser
 # read the data from the excel spreadsheet
 def Sort(sub_li):
     sub_li.sort(key = lambda x: x[1])
     return sub_li
-data = openpyxl.load_workbook(filename = 'banddata.xlsx')
-data.iso_dates = True
+data = openpyxl.load_workbook(filename = 'banddata.xlsx') # opens up predetermined file
+data.iso_dates = True #get dates into format I want
 sheet = data.active
-x = 4
-thelist = []
-while x<37 :
-    artist = sheet['C'+str(x)].value
-    date = sheet['D'+str(x)].value
-    dt = datetime.strptime(str(date),"%Y-%m-%d %H:%M:%S")
-    location = sheet['E'+str(x)].value
-    jawn = (artist, dt, location)
-    thelist.append(jawn)
-    x = x + 1
-thelist = Sort(thelist)
-print(thelist)
+x = 4 # starting line for reader to begin reading
+thelist = [] # creates a list for the events
+while x<37 : # end value where you want the reader to stop reading from
+    artist = sheet['C'+str(x)].value # get artist starting
+    date = sheet['D'+str(x)].value # get initial date value
+    dt = datetime.strptime(str(date),"%Y-%m-%d %H:%M:%S") #getd date into format that can be >'d
+    location = sheet['E'+str(x)].value # get location string
+    jawn = (artist, dt, location) # put all 3 as an object for list
+    thelist.append(jawn) # puts object in list
+    x = x + 1 # iterating element because I'm not the best at Python.
+thelist = Sort(thelist) # sorts the lsit in order of date, mixing up previously seperated bands to make for easier time sorting
+print(thelist) # shows the list in all it's glory
