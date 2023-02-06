8471ab437be1a06546cabdbb0f37d13f61e4e345
Michael Mandich <85703638+michaelmandich@users.noreply.github.com>
2023-02-05 19:45:39 -0500

    Update SpreadReader.py
    updated and functional, change the number 37 to the final number in your spreadsheet, or add to mine. currently sorts all events in order of date.

--- SpreadReader.py
+++ SpreadReader.py
@@ -1,24 +1,24 @@
 import openpyxl
 from openpyxl import Workbook
-
-# read the data from the excel spreadsheet 
+from datetime import datetime
+from datetime import timedelta
+from dateutil import parser
+# read the data from the excel spreadsheet
+def Sort(sub_li):
+    sub_li.sort(key = lambda x: x[1])
+    return sub_li
 data = openpyxl.load_workbook(filename = 'banddata.xlsx')
-print(data.sheetnames)
 data.iso_dates = True
 sheet = data.active
-print (sheet['C4'].value)
-x = 3
+x = 4
 thelist = []
 while x<37 :
     artist = sheet['C'+str(x)].value
     date = sheet['D'+str(x)].value
-    print(date)
+    dt = datetime.strptime(str(date),"%Y-%m-%d %H:%M:%S")
     location = sheet['E'+str(x)].value
-    jawn = (artist, date, location)
+    jawn = (artist, dt, location)
     thelist.append(jawn)
     x = x + 1
-    
-
-# loop through each row in the data
-# print(thelist)
-# print the list of objects 
+thelist = Sort(thelist)
+print(thelist)
